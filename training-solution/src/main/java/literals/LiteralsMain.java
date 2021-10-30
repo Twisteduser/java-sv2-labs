d68d85f0f7f4e4b585b27d930d2bb1cfcef6bf2b
Twisteduser <twistrdroid@gmail.com>
2021-10-30 23:50:39 +0200

    literals tasks done

---
+++ training-solution/src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,25 @@
+package literals;
+
+public class LiteralsMain {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        System.out.println( a + " " + b);
+        System.out.println(Integer.toString(a) + Integer.toString(b));
+
+        double quotient = 3/4d;
+        System.out.println(quotient);
+
+        long big = 3_244_444_444L;
+        System.out.println(big);
+
+        String word = "title".toUpperCase();
+        System.out.println(word);
+
+        System.out.println(Integer.toBinaryString(20));
+        System.out.println(Integer.toBinaryString(40));
+        
+
+    }
+}
