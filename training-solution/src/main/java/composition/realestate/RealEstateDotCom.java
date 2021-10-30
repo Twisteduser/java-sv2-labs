884f200b12c1695b3cb0aed9092105606eb9b6bb
Twisteduser <twistrdroid@gmail.com>
2021-10-30 20:22:21 +0200

    composition / realstate task done

--- training-solution/src/main/java/composition/realestate/RealEstateDotCom.java
+++ training-solution/src/main/java/composition/realestate/RealEstateDotCom.java
@@ -2,6 +2,13 @@ package composition.realestate;
 
 public class RealEstateDotCom {
     public static void main(String[] args) {
-        
+        Address address = new Address("Budapest", "1115", "Fehérvári út",1);
+        System.out.println(address);
+
+        Details details = new Details("Zajos, meleg 9. emeleti lakás", address, 50,0);
+        System.out.println(details);
+
+        RealEstate realEstate = new RealEstate("9. emeleti lakás", 60,details);
+        System.out.println(realEstate);
     }
 }
