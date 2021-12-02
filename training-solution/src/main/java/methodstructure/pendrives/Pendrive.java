20c4b4c2d3a69d0fcc6497e447fd940e9e23fa26
Twisteduser <twistrdroid@gmail.com>
2021-12-02 19:58:39 +0100

    methodstructure done

---
+++ training-solution/src/main/java/methodstructure/pendrives/Pendrive.java
@@ -0,0 +1,57 @@
+package methodstructure.pendrives;
+
+public class Pendrive {
+    private String name;
+    private int capacity;
+    private int price;
+
+    public Pendrive(String name, int capacity, int price) {
+        this.name = name;
+        this.capacity = capacity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Pendrives{" +
+                "name='" + name + '\'' +
+                ", capacity=" + capacity +
+                ", price=" + price +
+                '}';
+    }
+    public void risePrice(int percent){
+        this.price += price*(percent/100);
+    }
+
+    public int comparePricePerCapacity(Pendrive other) {
+        double ppc = getPricePerCapacity();
+        double otherPpc = other.getPricePerCapacity();
+        if (ppc > otherPpc) {
+            return 1;
+        }
+        if (ppc < otherPpc) {
+            return -1;
+        }
+        return 0;
+    }
+
+    public boolean isCheaperThan(Pendrive other) {
+        return this.price < other.price;
+    }
+
+    private double getPricePerCapacity() {
+        return (double) price / capacity;
+    }
+}
