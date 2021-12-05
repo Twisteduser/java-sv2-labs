19be8c2fcfb0a18ddb7415341572260adf6e8987
Twisteduser <twistrdroid@gmail.com>
2021-12-05 21:15:36 +0100

    staticattrmeth done

---
+++ training-solution/src/test/java/staticattrmeth/Vet/DogTest.java
@@ -0,0 +1,15 @@
+package staticattrmeth.Vet;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DogTest {
+    @Test
+    void testCreate() {
+        Dog dog = new Dog("Buksi", 8, Species.GERMAN_SHEPHERD);
+
+        assertEquals("Buksi", dog.getName());
+        assertEquals(8, dog.getAge());
+        assertEquals(Species.GERMAN_SHEPHERD, dog.getSpecies());
+        assertEquals(0L, dog.getCodeNumber());
+    }
+}
