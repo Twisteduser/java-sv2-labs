2367aa36372167431ffae113d4b769192c245cc6
Twisteduser <twistrdroid@gmail.com>
2021-12-12 21:08:12 +0100

    inheritancemethods done

---
+++ training-solution/src/main/java/inheritancemethods/plane/FirstClassPassenger.java
@@ -0,0 +1,16 @@
+package inheritancemethods.plane;
+
+public class FirstClassPassenger extends PriorityPassenger{
+    private final int extraCharge = 20000;
+
+    public FirstClassPassenger(String name, int priceOfPlaneTicket, int percent) {
+        super(name, priceOfPlaneTicket, percent);
+    }
+
+    public int getExtraCharge() {
+        return extraCharge;
+    }
+    public int getPriceOfPlaneTicket(){
+        return (int) (super.getPriceOfPlaneTicket() + extraCharge);
+    }
+}
